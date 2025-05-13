22d94dcca187b020aa5aae4aa99e7ef5f5dd81e9
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-05-14 01:04:36 +0200

    Update PeanoNat.lean

--- PeanoNat.lean
+++ PeanoNat.lean
@@ -1495,6 +1495,7 @@ theorem add_eq_zero_iff (a b : PeanoNat) :
 --      FALLA COMO SE ESPERABA
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 -- Pseudocódigo conceptual
   def count_subtractions_aux
       (current_n m : PeanoNat) (count_so_far : PeanoNat) (neq_0 : m ≠ zero): PeanoNat :=
@@ -1619,11 +1620,16 @@ theorem add_eq_zero_iff (a b : PeanoNat) :
   theorem substract_ge_0_then_terminate(n m: PeanoNat) (neq_0: m ≠ zero):
       ∃ count : PeanoNat
 =======
+=======
+>>>>>>> Stashed changes
 /-   def repeat_substract (n m k: PeanoNat) (h_le: m <= n) : PeanoNat :=
     | succ n', zero => succ n'
     | succ n', succ m' =>
       repeat_substract n' m' (le_of_succ_le_succ h_le)
 
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
   theorem substract_ge_0_then_terminate(n m: PeanoNat) (neq_0: m ≠ zero):
       ∃ count : PeanoNat
